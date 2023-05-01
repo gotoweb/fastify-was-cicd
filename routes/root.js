35ce418cd62bf3b63709389d80a8b4b4fcf57359
Hoyong Lee <igotoweb@gmail.com>
2023-05-01 17:29:50 +0900

    print env

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+    return { root: true, hostname: process.env.RDS_HOSTNAME, username: process.env.RDS_USERNAME }
   })
 }
